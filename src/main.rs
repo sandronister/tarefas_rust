26e405ae5686dcaa4b5a1a1d004f77c2a04d9791
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-17 09:12:00 -0300

    feat:number is even

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 
-fn get_major(list :&Vec<i32>) -> i32 {
-    let mut major = list[0];
-     
-     for item in list.iter().skip(1) {
-        if item > &major{
-            major = *item;
-        }
-     }
-    major
+fn is_even(number_i32:i32) -> bool {
+   for i in 2..(number_i32-1) {
+         if number_i32 % i == 0 {
+              return false;
+         }
+   }
+   return true;
 }
 
 
 fn main() {
 
-    let list = vec![1, 2, 3, 9, 5, 5, 5, 5, 5, 8, 5, 5, 5];
-    let major = get_major(&list);
-    println!("Majority element is: {}", major);
+   for i in 1..100 {
+       if is_even(i) {
+           println!("{} is even", i);
+       } else {
+           println!("{} is odd", i);
+       }
+   }
 }
