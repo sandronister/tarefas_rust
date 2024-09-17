a47a48edbd318d052d16f2e22d778121d70aa45c
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-17 10:11:56 -0300

    feat:table mult

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
+use std::io;
 
-fn is_even(number_i32:i32) -> bool {
-   for i in 2..(number_i32-1) {
-         if number_i32 % i == 0 {
-              return false;
-         }
-   }
-   return true;
+
+fn table_mult(number_i32: i32) -> [i32; 11] {
+    let mut table = [1; 11];
+    for i in 0..11 {
+        if i != 0 {
+            table[i] = number_i32 * (i as i32);
+        }
+       
+    }
+    table
 }
 
 
 fn main() {
-
-   for i in 1..100 {
-       if is_even(i) {
-           println!("{} is even", i);
-       } else {
-           println!("{} is odd", i);
-       }
-   }
+    println!("Enter a number: ");
+    let mut read_number    = String::new();
+    io::stdin().read_line(&mut read_number).expect("Failed to read line");
+    let number = read_number.trim().parse::<i32>().unwrap();
+    let table = table_mult(number);
+    for i in 1..11 {
+        println!("{} x {} = {}", number, i, table[i]);
+    }
+   
 }
