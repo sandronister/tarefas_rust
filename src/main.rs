b8a61738298cd2f3837f91c55f839fb3c4f339c8
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-18 08:34:43 -0300

    feat:score average

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@ use core::f32;
 use std::io;
 
 
-fn real_sum(n: &[f32]) -> f32 {
+fn calculate_average(n: &[f32]) -> f32 {
     let mut sum = 0.0;
     for i in n.iter() {
         sum += i;
     }
-    sum
+    let media=sum/n.len() as f32;
+    media
 }
 
 
 fn main() {
         let mut n = String::new();
-        println!("Enter the numbers separated by space: ");
+        println!("Enter the score separated by space: ");
         io::stdin().read_line(&mut n).expect("Failed to read line");
         let n: Vec<f32> = n.trim().split_whitespace().map(|num| num.parse().unwrap()).collect();
-        let sum = real_sum(&n);
-        println!("The sum of the numbers is: {}", sum);
+        let sum = calculate_average(&n);
+        println!("The media of the score: {}", sum);
 }
