a1a0f04f2c4b2bf8b13460f73581601a3da26c4e
Julio Sandroni <sandronister@gmail.com>
2025-01-04 22:59:48 -0300

    fix:prime number task

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 
-fn is_even(number_i32:i32) -> bool {
-   for i in 2..(number_i32-1) {
-         if number_i32 % i == 0 {
-              return false;
-         }
-   }
-   return true;
+fn prime_number(number_i32:i32) -> bool {
+    if number_i32 < 2 {
+        return false;
+    }
+    for i in 2..number_i32 {
+        if number_i32 % i == 0 {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 fn main() {
 
-   for i in 1..100 {
-       if is_even(i) {
-           println!("{} is even", i);
-       } else {
-           println!("{} is odd", i);
-       }
-   }
+    let max = 10000000;
+    for i in 0..max {
+        if prime_number(i) {
+            println!("{} is a prime number", i);
+        }
+    }
 }
