d547e21f6b1edcb4d77332010a2b0cbf9d21b111
Julio Sandroni <sandronister@gmail.com>
2025-01-04 23:23:37 -0300

    fix:mean float numbers

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 use std::io;
 
 
-fn table_mult(number_i32: i32) -> [i32; 11] {
-    let mut table = [1; 11];
-    for i in 0..11 {
-        if i != 0 {
-            table[i] = number_i32 * (i as i32);
-        }
-       
+fn mean(table: &Vec<f32>) -> f32 {
+    let mut sum = 0.0;
+    for i in table {
+        sum += i;
     }
-    table
+    sum 
 }
 
 
 fn main() {
-    println!("Enter a number: ");
-    let mut read_number    = String::new();
-    io::stdin().read_line(&mut read_number).expect("Failed to read line");
-    let number = read_number.trim().parse::<i32>().unwrap();
-    let table = table_mult(number);
-    for i in 1..11 {
-        println!("{} x {} = {}", number, i, table[i]);
+    let mut table: Vec<f32> = Vec::new();
+    loop {
+        println!("Enter a number: ");
+        let mut read_number    = String::new();
+        
+        io::stdin().read_line(&mut read_number).expect("Failed to read line");
+
+        if read_number.trim().is_empty() {
+            let total = mean(&table);
+            println!("The mean of the numbers is: {}", total);
+            break;
+        }
+            
+
+        let number: f32 = match read_number.trim().parse::<f32>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+            
+        };
+        table.push(number)
     }
    
 }
